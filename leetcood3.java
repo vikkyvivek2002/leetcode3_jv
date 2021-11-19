c1758ac16fe6ee02f2384521eccc366c65f9ca53
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-11-19 23:22:05 +0530

    Add files via upload

---
+++ leetcood3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+      int temp =0;
+      for(int i =0; i<nums.length;i++){
+          for(int j =0; j<i;j++){
+               if(nums[i] == nums[j])
+                  temp++;
+          }
+      }
+                 return temp;
+    }
+}
